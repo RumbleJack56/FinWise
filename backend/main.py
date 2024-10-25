ceacbf2c46ed47d748f87e9a279cae5574cdb0c1
RumbleJack56 <ujjwalkakar2005@gmail.com>
2024-10-26 01:46:36 +0530

    Ondemand hitter

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from routers import users
 from routers import stocks
-
+from routers import getondemand
 app = FastAPI()
 
 app.include_router(users.router, prefix="/user")
 app.include_router(stocks.router, prefix= "/stocks")
-
+app.include_router(getondemand.router, prefix="/getondemand")
 @app.get("/")
 async def root():
     return {"message": "Root Node"}
---
+++ backend/routers/getondemand.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from datetime import date, datetime
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+router = APIRouter()
+supabase: Client = create_client(os.environ.get("SUPABASE_URL"), os.environ.get("SUPABASE_KEY"))
+last_data = ""
+@router.get("/hit_od")
+async def hit_od(data : str):
+    last_data = data
+    return {"message": "Stock added successfully", "data": ""}
+
+#fetchinggg
+@router.get("/read_od")
+async def read_od():
+    
+    return {"data": "last_data"}
+    
+    
